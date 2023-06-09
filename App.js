38b6e86880b89c38dbddd5e99fd5894f25aec8e5
jcarloevan <91675195+jcarloevan@users.noreply.github.com>
2023-06-09 12:10:36 +0100

    Version 0.2 set up of native react application

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
+npimport { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/task'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Carlo's Tasks</Text>
+        <StatusBar style="auto" />
+
+        <View style={styles.items}>
+        {/*--------TASKS---------- */}
+        <Task text={'Place Holder 1'}></Task>
+        <Task text={'Place Holder 2'}></Task>
+        <Task text={'Place Holder 3'}></Task>
+        </View>
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#82b6ba',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 80
   },
+  sectionTitle:{
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  taskWrapper:{
+    paddingTop: 20,
+    paddingHorizontal: 'bold',
+    marginBottom: 10,
+  },
+  items: {
+    paddingTop: 20,
+  }
 });
